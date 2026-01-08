e88fc47bcb904d5a031f6186499f4a9eaac6a312
DigBugReal <greengogetter4real@gmail.com>
2026-01-07 21:09:14 -0500

    disable prints

--- Lua/items/crackedPearl.lua
+++ Lua/items/crackedPearl.lua
@@ -19,56 +19,56 @@ Callback.add(purl.on_acquired, function(actor)
 				local stacks = actor:item_count(purl)
 				if stacks <= 0 then return end
 					api.hp_regen_add(0.08 + (0.02 * stacks))
-					print("regen")
+					-- print("regen")
 			end)
 		elseif actor.pearlRoll == 7 then
 			RecalculateStats.add(function(actor, api)
 				local stacks = actor:item_count(purl)
 				if stacks <= 0 then return end
 					api.armor_mult(1 + (0.1 * stacks))
-					print("armor")
+					-- print("armor")
 			end)
 		elseif actor.pearlRoll == 6 then
 			RecalculateStats.add(function(actor, api)
 				local stacks = actor:item_count(purl)
 				if stacks <= 0 then return end
 					api.attack_speed_add(0.1 * stacks)
-					print("AtSpeed")
+					-- print("AtSpeed")
 			end)
 		elseif actor.pearlRoll == 5 then
 			RecalculateStats.add(function(actor, api)
 				local stacks = actor:item_count(purl)
 				if stacks <= 0 then return end
 					api.critical_chance_add(10 * stacks)
-					print("Crit")
+					-- print("Crit")
 			end)
 		elseif actor.pearlRoll == 4 then
 			RecalculateStats.add(function(actor, api)
 				local stacks = actor:item_count(purl)
 				if stacks <= 0 then return end
 					api.pHmax_add(0.2 * stacks)
-					print("Speed is key")
+					-- print("Speed is key")
 			end)
 		elseif actor.pearlRoll == 3 then
 			RecalculateStats.add(function(actor, api)
 				local stacks = actor:item_count(purl)
 				if stacks <= 0 then return end
 					api.pVmax_add(0.4 * stacks)
-					print("boing")
+					-- print("boing")
 			end)
 		elseif actor.pearlRoll == 2 then
 			RecalculateStats.add(Callback.Priority.AFTER, function(actor, api)
 				local stacks = actor:item_count(purl)
 				if stacks <= 0 then return end
 					api.damage_mult(1 + (0.1 * stacks))
-					print("damge")
+					-- print("damge")
 			end)
 		elseif actor.pearlRoll == 1 then
 			RecalculateStats.add(Callback.Priority.AFTER, function(actor, api)
 				local stacks = actor:item_count(purl)
 				if stacks <= 0 then return end
 					api.maxhp_mult(1 + (0.1 * stacks))
-					print("Soul !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+					-- print("Soul !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 			end)
 		end
 	end
